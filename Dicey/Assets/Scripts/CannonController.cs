20ade9f32ea2e3a9a085722f5996a166058d167a
Lukas Saul <lukas.saul@web.de>
2022-07-17 20:15:33 +0200

    Added cannon shooting sound.

--- Dicey/Assets/Scripts/CannonController.cs
+++ Dicey/Assets/Scripts/CannonController.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class CannonController : MonoBehaviour
 {
     public float ShootingForce = 6000f;
     public ParticleSystem SmokeParticles;
+
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             SmokeParticles.Play();
+            _audioSource.Play();
             other.attachedRigidbody.AddForce(ShootingForce * transform.forward);
         }
     }
