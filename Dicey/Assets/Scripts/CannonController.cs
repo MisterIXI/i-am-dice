87571fe392a988724d49ac008b4f2c4ce2967a97
SteffWin <steffen.winternheimer@gmail.com>
2022-07-17 18:25:31 +0200

    Update cannon particles

--- Dicey/Assets/Scripts/CannonController.cs
+++ Dicey/Assets/Scripts/CannonController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class CannonController : MonoBehaviour
 {
     public float ShootingForce = 6000f;
+    public ParticleSystem SmokeParticles;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             Debug.Log("Player collided with cannon trigger.");
+            SmokeParticles.Play();
             other.attachedRigidbody.AddForce(ShootingForce * transform.forward);
         }
     }
