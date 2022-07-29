4858551cac7166e2ce104f34322043e123051dc7
Yannik <yannik.braendle@t-online.de>
2022-07-29 21:36:18 +0200

    car prototype

---
+++ Dicey/Assets/Scripts/CarController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("Player has entered the car!");
+        }
+    }
+}
