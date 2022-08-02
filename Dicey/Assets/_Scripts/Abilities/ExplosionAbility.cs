b955de89bb53ba60313fa24b6cbee318bca3304e
Yannik <yannik.braendle@t-online.de>
2022-08-03 01:08:20 +0200

    Add Explosionability

--- Dicey/Assets/_Scripts/Abilities/ExplosionAbility.cs
+++ Dicey/Assets/_Scripts/Abilities/ExplosionAbility.cs
@@ -7,7 +7,13 @@ using UnityEngine.InputSystem;
 
 public class ExplosionAbility : Ability
 {
-
+    public float ExplosionRadius = 25;
+    public float ExplosionForce = 3000;
+    private GameObject _explosionParticles;
+    private void Start()
+    {
+        _explosionParticles = Resources.Load<GameObject>("ExplosionParticles");
+    }
     public override void Select()
     {
         Debug.Log("ExplosionAbility Selected");
@@ -25,6 +31,32 @@ public class ExplosionAbility : Ability
 
     public override void AbilityAction(InputAction.CallbackContext context)
     {
-        Debug.Log("ExplosionAbility AbilityAction");
+        Collider[] hits = Physics.OverlapSphere(transform.position, ExplosionRadius);
+        for (int i = 0; i < hits.Length; i++)
+        {
+            if (hits[i].tag != "Player" && hits[i].tag != "DiceFace")
+            {
+                if (hits[i].TryGetComponent(out Rigidbody rb))
+                {
+                    rb.AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius);
+                }
+                else if (hits[i].transform.parent.TryGetComponent(out Rigidbody rb2))
+                {
+                    rb2.AddExplosionForce(ExplosionForce, transform.position, ExplosionRadius);
+                }
+            }
+        }
+        GameObject go = Instantiate(_explosionParticles, transform.position, Quaternion.identity);
+        go.transform.localScale = new Vector3(ExplosionRadius * 0.1f, ExplosionRadius * 0.1f, ExplosionRadius * 0.1f);
+        Destroy(go, 2);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (enabled)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, ExplosionRadius);
+        }
     }
 }
